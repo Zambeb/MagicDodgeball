aa9f150845e6d0a22942ec3d96841809fef8c73a
Timur <erdashev@gmail.com>
2025-07-12 18:55:36 +0200

    main menu and titles

---
+++ Assets/Scripts/7. UI/MainMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+    public GameObject loadingScreenPanel;
+
+    [Header("Gameplay")]
+    public string gameSceneName = "GameScene";
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadGameAsync());
+    }
+    
+    private IEnumerator LoadGameAsync()
+    {
+        mainMenuPanel.SetActive(false);
+        loadingScreenPanel.SetActive(true);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(gameSceneName);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
